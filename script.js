bd7b173ae928a25c327c760bd8d1665c722fe852
cbg1991 <christiangaillard@gmail.com>
2022-12-11 17:37:05 -0800

    added shadows to index buttons

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 console.log("test");
-
+/* scroll progress bar */
 let bar = document.querySelector("#progressbar");
   window.addEventListener("scroll", () => {
     let max = document.body.scrollHeight - innerHeight;
     bar.style.width = `${(pageYOffset / max) * 100}%`;
   });
+
+/* hover shadows for index buttons */
+let shadow = document.querySelectorAll(".homeButton");
+for(i = 0; i < shadow.length; i++) {
+    let shadowImage = shadow[i];
+        shadowImage.addEventListener("mouseover", addShadow, false);
+        shadowImage.addEventListener("mouseout", removeShadow, false);
+        function addShadow() {
+            shadowImage.setAttribute('id','hovered');
+        }
+        function removeShadow() {
+            shadowImage.removeAttribute('id','hovered');
+        }
+}
+
 
 
